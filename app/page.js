5bc840aee76e9fca7490f2a44b118b65b2699d56
dongchen <166428054+ding-dong-qiang@users.noreply.github.com>
2024-09-20 22:34:51 -0600

    week3 assignment
    1,title change(create next app to shopping list)
    2, shopping item, shopping list
    3, styling

--- app/page.js
+++ app/page.js
@@ -1,16 +1,19 @@
 import Link from "next/link";
 
 export default function Home() {
+  let linkStyles = "underline text-cyan-600 hover:text-cyan-300";
   return (
     <main>
       <h1 className="text-3xl">CPRG 306: Web Development 2 - Assignments</h1>
       <ul>
         <li>
-          <Link
-            href="./week-2/"
-            className="underline text-cyan-600 hover:text-cyan-300"
-          >
-            Week 2 Assignment
+          <Link href="./week-2/" className={linkStyles}>
+            Week 2 Introduction to React
+          </Link>
+        </li>
+        <li>
+          <Link href="./week-3/" className={linkStyles}>
+            Week 3 Objects
           </Link>
         </li>
       </ul>
---
+++ app/week-3/page.js
@@ -0,0 +1,11 @@
+import ItemList from "./item-list";
+
+export default function Page() {
+  return (
+    <main className="bg-slate-600">
+      <h1 className="text-3xl font-bold m-3">Shopping List</h1>
+      <ItemList />
+
+    </main>
+  );
+}
